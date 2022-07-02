cced232615b16011091b5d92273d39a7ed071488
unknown <kylelarsen12@gmail.com>
2022-07-01 21:13:06 -0400

    Add remaining stub routes and readme

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+//Route through places.js
+app.use("/places", require("./controllers/places"));
+
 //homepage route
 app.get("/", (req, res) => {
   res.send("Hello World");
