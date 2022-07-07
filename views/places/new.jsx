bbd812285822ee5cb4d562f72585447bf740fe27
unknown <kylelarsen12@gmail.com>
2022-07-07 17:52:48 -0400

    Add and render new view

---
+++ views/places/new.jsx
@@ -0,0 +1,16 @@
+//require modules
+const React = require("react");
+const Def = require("../default");
+
+//new form
+function newForm() {
+  return (
+    <Def>
+      <main>
+        <h1>Add a new place</h1>
+      </main>
+    </Def>
+  );
+}
+
+module.exports = newForm;
