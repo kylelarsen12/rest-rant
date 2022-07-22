8e7e40b64ad33eeb9f98b7c1e52234e86f3e172f
unknown <kylelarsen12@gmail.com>
2022-07-22 15:13:08 -0400

    Add comments seed data for place

--- seeders/seed-comments.js
+++ seeders/seed-comments.js
@@ -1,2 +1,27 @@
 //Dependencies
 const db = require("../models");
+
+//Create seed data
+async function seed() {
+  //Get h-thai-ml place
+  let place = await db.Place.findOne({ name: "H-Thai-ML" });
+
+  //Create fake comment
+  let comment = await db.Comment.create({
+    author: "Joe Mama",
+    rant: false,
+    stars: 3.5,
+    content: "Joe? Joe who?",
+  });
+
+  //Add comment to place's comment array
+  place.comments.push(comment.id);
+
+  //Save place with new comment
+  await place.save();
+
+  //Exit
+  process.exit();
+}
+
+seed();
